9e58873dc09fe1816a8744bf411484f19c3da113
Apra <apra.khanna2304@gmail.com>
2025-12-04 18:22:34 +0530

    build REST API with path/query param filtering and response utilities

--- utils/sendJSONResponse.js
+++ utils/sendJSONResponse.js
@@ -1,5 +1,9 @@
 export const sendJSONResponse = (res, statusCode, payload) => {
     res.setHeader('Content-Type', 'application/json')
+    // Any frontend (any domain) is allowed to call my API.
+//Browser is told that only GET requests are permitted to this API.
+       res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET')
     res.statusCode = statusCode
     res.end(JSON.stringify(payload))
 }
