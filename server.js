af2703daaa6393a0050164d0b07b52a2335840d0
Apra <apra.khanna2304@gmail.com>
2025-12-04 15:59:20 +0530

    added filtered functionality and refactored the code

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 import http from "node:http";
 import { getDataFromDB } from "./db.js";
+import { sendJSONResponse } from './utils/sendJSONResponse.js'
+
 const PORT = 8000;
 const server = http.createServer(async (req, res) => {
   const destinations = await getDataFromDB();
   if (req.url === "/api" && req.method === "GET") {
     // res.end("This is from the server");
-    res.setHeader("Content-Type", "application/json");
-    res.statusCode=200
-    res.end(JSON.stringify(destinations));
-  }else {
-     res.setHeader('Content-Type', 'application/json')
-    res.statusCode = 404
-    res.end(JSON.stringify( {error: "not found", message: "The requested route does not exist"}))
+       sendJSONResponse(res, 200, destinations)
+   
+  } else if (req.url.startsWith("/api/continent") && req.method === "GET") {
+    const continent = req.url.split("/").pop();
+
+    const filteredData = destinations.filter((destination) => {
+      return destination.continent.toLowerCase() === continent.toLowerCase();
+    });
+  sendJSONResponse(res, 200, filteredData)
+    
+  } else {
+     sendJSONResponse(res, 404, ({
+      error: "not found",
+      message: "The requested route does not exist"
+    })
+    )
   }
   //     res.write("this is some data")
   // res.end("hello from server!", 'utf8', ()=> console.log('response end'))
