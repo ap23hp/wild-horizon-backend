f5958abc256966d2fe2d81e4a215a392d93e7df3
Apra <apra.khanna2304@gmail.com>
2025-12-04 16:58:47 +0530

    refactored again with some DRY

--- server.js
+++ server.js
@@ -1,28 +1,33 @@
 import http from "node:http";
 import { getDataFromDB } from "./db.js";
-import { sendJSONResponse } from './utils/sendJSONResponse.js'
+import { sendJSONResponse } from "./utils/sendJSONResponse.js";
+import { getDataByPathParams } from "./utils/getDataByPathParams.js";
 
 const PORT = 8000;
 const server = http.createServer(async (req, res) => {
   const destinations = await getDataFromDB();
   if (req.url === "/api" && req.method === "GET") {
     // res.end("This is from the server");
-       sendJSONResponse(res, 200, destinations)
-   
+    sendJSONResponse(res, 200, destinations);
   } else if (req.url.startsWith("/api/continent") && req.method === "GET") {
     const continent = req.url.split("/").pop();
+    const filteredData = getDataByPathParams(
+      destinations,
+      "continent",
+      continent
+    );
 
-    const filteredData = destinations.filter((destination) => {
-      return destination.continent.toLowerCase() === continent.toLowerCase();
-    });
-  sendJSONResponse(res, 200, filteredData)
-    
+    sendJSONResponse(res, 200, filteredData);
+  } else if (req.url.startsWith("/api/country") && req.method === "GET") {
+    const country = req.url.split("/").pop();
+    const filteredData = getDataByPathParams(destinations, "country", country);
+
+    sendJSONResponse(res, 200, filteredData);
   } else {
-     sendJSONResponse(res, 404, ({
+    sendJSONResponse(res, 404, {
       error: "not found",
-      message: "The requested route does not exist"
-    })
-    )
+      message: "The requested route does not exist",
+    });
   }
   //     res.write("this is some data")
   // res.end("hello from server!", 'utf8', ()=> console.log('response end'))
