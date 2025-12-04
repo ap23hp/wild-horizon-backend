c16a9599c3ee8ea2343110a3320b003d52c89529
Apra <apra.khanna2304@gmail.com>
2025-12-04 15:13:50 +0530

    sending request status code and header

---
+++ db.js
@@ -0,0 +1,7 @@
+import { data } from "./data.js";
+
+export async function getDataFromDB() {
+  return data;
+}
+
+//we are mimicing accessing data from database
--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-import http from 'node:http'
-const PORT = 8000
-const server=http.createServer((req,res)=>{
-res.end("hello from server!")
-})
+import http from "node:http";
+import { getDataFromDB } from "./db.js";
+const PORT = 8000;
+const server = http.createServer(async (req, res) => {
+  const destinations = await getDataFromDB();
+  if (req.url === "/api" && req.method === "GET") {
+    // res.end("This is from the server");
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode=200
+    res.end(JSON.stringify(destinations));
+  }else {
+     res.setHeader('Content-Type', 'application/json')
+    res.statusCode = 404
+    res.end(JSON.stringify( {error: "not found", message: "The requested route does not exist"}))
+  }
+  //     res.write("this is some data")
+  // res.end("hello from server!", 'utf8', ()=> console.log('response end'))
+});
 
-server.listen(PORT,()=>console.log(`server running on port: ${PORT}`))
+server.listen(PORT, () => console.log(`server running on port: ${PORT}`));
